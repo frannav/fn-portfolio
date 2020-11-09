4bd646fcb6421e5a37946936e1c850fe3ee43c4f
Fran Nav <frannavmart@gmail.com>
2020-11-09 09:52:37 +0000

    Remove lib

--- components/ListOfProjectsCards/index.js
+++ components/ListOfProjectsCards/index.js
@@ -1,16 +1,18 @@
 import ProjectCard from '../ProjectCard';
 import { CardsContent } from './styles';
 
-export default function ListOfProjectsCards() {
+export default function ListOfProjectsCards({ data }) {
+    console.log(data)
     return (
         <CardsContent>
             <h1>I´m ListOfProjectsCards </h1>
             <ProjectCard title='Test props title!' photo='animoji2.png' />
             {
-                [1,2,3,4].map((item) => {
+                [1,2,3].map((item) => {
                     return <ProjectCard title={item.title} photo={item.photo}/>
                 })
             }
         </CardsContent>
+        
     )
 }
