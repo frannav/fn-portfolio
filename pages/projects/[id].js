cbb157b9377ce7f76c56f704f6667ebde7d97e25
Fran Nav <frannavmart@gmail.com>
2021-01-15 14:21:51 +0000

    Fix some npm run build errors

--- pages/projects/[id].js
+++ pages/projects/[id].js
@@ -1,89 +1,94 @@
-import Head from 'next/head';
-import Link from 'next/link';
-import { getAllIds, getDataFromId } from '../../lib/markdown';
-import NavBar from '../../components/NavBar';
-import Footer from '../../components/Footer';
-import {
-    Container,
-    Title,
-    TagsWrapper,
-    Tag,
-    Body,
-    MarkdownContent,
-    MarkdownImage,
-    Content,
-    CardLink,
-    DemoLink
-} from './styles';
-import { FaGithub, FaRegWindowMaximize } from 'react-icons/fa'
+// import Head from 'next/head';
+// import Link from 'next/link';
+// import { getAllIds, getDataFromId } from '../../lib/markdown';
+// import NavBar from '../../components/NavBar';
+// import Footer from '../../components/Footer';
+// import {
+//     Container,
+//     Title,
+//     TagsWrapper,
+//     Tag,
+//     Body,
+//     MarkdownContent,
+//     MarkdownImage,
+//     Content,
+//     CardLink,
+//     DemoLink
+// } from './styles';
+// import { FaGithub, FaRegWindowMaximize } from 'react-icons/fa'
 
-export default function Project({ fileData }) {
-    let demoLink
-    if (fileData.demo) {
-        demoLink =
-        <Link href={fileData.demo}
-        passHref>
-            <DemoLink target="_blank" rel="noopener noreferrer">
-                <FaRegWindowMaximize size='27px' />
-                Demo
-            </DemoLink> 
-        </Link>
-    } else {
-        demoLink = <h3>Demo not available</h3>
-    }
+export default function Project() {
+  return (
+    <h1>🏗Section under construction</h1>
+  )
 
-    return (
-        <>
-            <Head>
-                <title>{fileData.title}</title>
-            </Head>
-            <NavBar />
-            <Container>
-                <Title>{fileData.title}</Title>
-                <TagsWrapper>
-                    {fileData.tags.map(tag => { return <Tag>{tag}</Tag> })}
-                    <Link
-                        href={fileData.github}
-                        passHref
-                    >
-                        <CardLink
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            <FaGithub size='27px' />
-                            View code on GitHub
-                        </CardLink>
-                    </Link>
-                    {demoLink}
-                </TagsWrapper>
-                <Body>
-                    <MarkdownContent>
-                        <Content dangerouslySetInnerHTML={{ __html: fileData.contentHtml }} />
-                    </MarkdownContent>
-                    <MarkdownImage
-                        src={`/${fileData.photo}`}
-                        alt={`Image from project ${fileData.title}`}
-                    />
-                </Body>
-            </Container>
-            <Footer />
-        </>
-    )
-}
+// export default function Project({ fileData }) {
+//     let demoLink
+//     if (fileData.demo) {
+//         demoLink =
+//         <Link href={fileData.demo}
+//         passHref>
+//             <DemoLink target="_blank" rel="noopener noreferrer">
+//                 <FaRegWindowMaximize size='27px' />
+//                 Demo
+//             </DemoLink> 
+//         </Link>
+//     } else {
+//         demoLink = <h3>Demo not available</h3>
+//     }
 
-export async function getStaticPaths() {
-    const paths = getAllIds()
-    return {
-        paths,
-        fallback: false
-    }
-}
+//     return (
+//         <>
+//             <Head>
+//                 <title>{fileData.title}</title>
+//             </Head>
+//             <NavBar />
+//             <Container>
+//                 <Title>{fileData.title}</Title>
+//                 <TagsWrapper>
+//                     {fileData.tags.map(tag => { return <Tag>{tag}</Tag> })}
+//                     <Link
+//                         href={fileData.github}
+//                         passHref
+//                     >
+//                         <CardLink
+//                             target="_blank"
+//                             rel="noopener noreferrer"
+//                         >
+//                             <FaGithub size='27px' />
+//                             View code on GitHub
+//                         </CardLink>
+//                     </Link>
+//                     {demoLink}
+//                 </TagsWrapper>
+//                 <Body>
+//                     <MarkdownContent>
+//                         <Content dangerouslySetInnerHTML={{ __html: fileData.contentHtml }} />
+//                     </MarkdownContent>
+//                     <MarkdownImage
+//                         src={`/${fileData.photo}`}
+//                         alt={`Image from project ${fileData.title}`}
+//                     />
+//                 </Body>
+//             </Container>
+//             <Footer />
+//         </>
+//     )
+// }
 
-export async function getStaticProps({ params }) {
-    const fileData = await getDataFromId(params.id)
-    return {
-        props: {
-            fileData
-        }
-    }
-}
+// export async function getStaticPaths() {
+//     const paths = getAllIds()
+//     return {
+//         paths,
+//         fallback: false
+//     }
+// }
+
+// export async function getStaticProps({ params }) {
+//     const fileData = await getDataFromId(params.id)
+//     return {
+//         props: {
+//             fileData
+//         }
+//     }
+// }
--- pages/projects/styles.js
+++ pages/projects/styles.js
@@ -1,109 +1,109 @@
-import styled from 'styled-components';
+// import styled from 'styled-components';
 
-export const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-    align-items: center;
-    width: 100%;
-`
+// export const Container = styled.div`
+//     display: flex;
+//     flex-direction: column;
+//     justify-content: flex-start;
+//     align-items: center;
+//     width: 100%;
+// `
 
-export const Title = styled.h1`
-    margin-top: 3.5rem;
-    font-size: 3rem;
-`
+// export const Title = styled.h1`
+//     margin-top: 3.5rem;
+//     font-size: 3rem;
+// `
 
-export const TagsWrapper = styled.div`
-    display: flex;
-    flex-direction: row;
-    margin-bottom: 1.5rem;
-    align-items: center;
-`
+// export const TagsWrapper = styled.div`
+//     display: flex;
+//     flex-direction: row;
+//     margin-bottom: 1.5rem;
+//     align-items: center;
+// `
 
-export const Tag = styled.h3`
-    margin-left: .5rem;
-    margin-right: .5rem;
-    & ::after {
-        content: '';
-        display: block;
-        width: 0;
-        height: 2px;
-        background: black;
-        transition: width .3s;
-    }
-    & :hover::after {
-        width: 100%;
-        transition: width .3s;
-    }
-`
+// export const Tag = styled.h3`
+//     margin-left: .5rem;
+//     margin-right: .5rem;
+//     & ::after {
+//         content: '';
+//         display: block;
+//         width: 0;
+//         height: 2px;
+//         background: black;
+//         transition: width .3s;
+//     }
+//     & :hover::after {
+//         width: 100%;
+//         transition: width .3s;
+//     }
+// `
 
-export const Body = styled.div`
-    padding: .8rem;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-`
+// export const Body = styled.div`
+//     padding: .8rem;
+//     display: flex;
+//     flex-direction: column;
+//     justify-content: center;
+//     align-items: center;
+// `
 
-export const MarkdownContent = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-    align-items: center;
-    margin-right: .5rem;
-`
+// export const MarkdownContent = styled.div`
+//     display: flex;
+//     flex-direction: column;
+//     justify-content: flex-start;
+//     align-items: center;
+//     margin-right: .5rem;
+// `
 
-export const MarkdownImage = styled.img`
-    max-width: 700px;
-    max-height: 900px;
-    width: auto;
-    height: auto;
-`
+// export const MarkdownImage = styled.img`
+//     max-width: 700px;
+//     max-height: 900px;
+//     width: auto;
+//     height: auto;
+// `
 
-export const Content = styled.div`
-    font-size: 1.3rem;
-`
+// export const Content = styled.div`
+//     font-size: 1.3rem;
+// `
 
-export const CardLink = styled.a`
-    color: ${({ theme }) => theme.colors.primary};
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    text-decoration: none;
-    margin-top: 1.5em;
-    margin-bottom: 1.5em;
-    margin-left: .5em;
-    margin-right: .5em;
-    padding: 3px 12px 3px 12px;
-    border-radius: 10px;
-    background-color: ${({ theme }) => theme.colors.secondary};
-    font-weight: 500;
-    transition: .1s;
-    width: auto;
-    & :hover {
-        color: #0E5F81;
-    }
-`
+// export const CardLink = styled.a`
+//     color: ${({ theme }) => theme.colors.primary};
+//     display: flex;
+//     flex-direction: row;
+//     align-items: center;
+//     text-decoration: none;
+//     margin-top: 1.5em;
+//     margin-bottom: 1.5em;
+//     margin-left: .5em;
+//     margin-right: .5em;
+//     padding: 3px 12px 3px 12px;
+//     border-radius: 10px;
+//     background-color: ${({ theme }) => theme.colors.secondary};
+//     font-weight: 500;
+//     transition: .1s;
+//     width: auto;
+//     & :hover {
+//         color: #0E5F81;
+//     }
+// `
 
 
 
-export const DemoLink = styled.a`
-    color: ${({ theme }) => theme.colors.primary};
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    text-decoration: none;
-    margin-top: 1.5em;
-    margin-bottom: 1.5em;
-    margin-left: .5em;
-    margin-right: .5em;
-    padding: 3px 12px 3px 12px;
-    border-radius: 10px;
-    background-color: ${({ theme }) => theme.colors.secondary};
-    font-weight: 500;
-    transition: .1s;
-    width: auto;
-    & :hover {
-        color: #0E5F81;
-    }
-`
+// export const DemoLink = styled.a`
+//     color: ${({ theme }) => theme.colors.primary};
+//     display: flex;
+//     flex-direction: row;
+//     align-items: center;
+//     text-decoration: none;
+//     margin-top: 1.5em;
+//     margin-bottom: 1.5em;
+//     margin-left: .5em;
+//     margin-right: .5em;
+//     padding: 3px 12px 3px 12px;
+//     border-radius: 10px;
+//     background-color: ${({ theme }) => theme.colors.secondary};
+//     font-weight: 500;
+//     transition: .1s;
+//     width: auto;
+//     & :hover {
+//         color: #0E5F81;
+//     }
+// `
